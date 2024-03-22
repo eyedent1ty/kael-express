52105b2ae6cbe1851c1c22dfe4b5644e50a78084
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-03-22 09:46:14 +0800

    implemented the searching functionality of products

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -3,7 +3,7 @@
   <VDivider></VDivider>
   <main class="product-list justify-center ga-4 my-8">
     <Product
-      v-for="product in productStore.productList"
+      v-for="product in props.products"
       :key="product.id"
       :title="product.title"
       :price="product.price"
@@ -18,6 +18,13 @@ const categoryStore = useCategoriesStore();
 
 productStore.fetchProducts();
 categoryStore.fetchCategories();
+
+const props = defineProps({
+  products: {
+    type: Array,
+    required: true
+  }
+});
 </script>
 
 <style scoped>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <ProductList />
+  <ProductList :products="products" />
 </template>
+
+<script setup lang="ts">
+const productStore = useProductStore();
+const route = useRoute();
+
+const products = computed(() => {
+  const keyword = route.query.keyword as string;
+  if (keyword !== null && keyword !== undefined) {
+    return productStore.productList.filter((product) => product.title.toLowerCase().includes(keyword.toLowerCase()));
+  }
+  return productStore.productList;
+})
+</script>
