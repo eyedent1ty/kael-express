83fda2f6692572180d7811e044fc4215dcd9327e
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-03-22 13:57:58 +0800

    added breadcrumbs for specific category route

--- pages/products/[category]/index.vue
+++ pages/products/[category]/index.vue
@@ -1,12 +1,36 @@
 <template>
-  <h1>{{ category }}</h1>
+  <VBreadcrumbs :items="items">
+    <template v-slot:divider>
+      <VIcon icon="mdi-chevron-right"></VIcon>
+    </template>
+  </VBreadcrumbs>
+
   <ProductList :products="filteredProducts" />
 </template>
 
 <script setup lang="ts">
+// Breadcrumbs
 const route = useRoute();
 const category = route.params.category as string;
 const productStore = useProductStore();
+
+const items = [
+  {
+    title: 'Home',
+    disabled: false,
+    href: '/'
+  },
+  {
+    title: 'Products',
+    disabled: false,
+    href: '/products'
+  },
+  {
+    title: category,
+    disabled: false,
+    href: `/products/${category}`
+  }
+];
 
 const filteredProducts = computed(() =>
   productStore.productList.filter(
