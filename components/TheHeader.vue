89f0d037a864d9ec8c6cc544324699510bd3c97f
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-03-22 14:12:34 +0800

    created another component for search input

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -31,31 +31,8 @@
           </div>
         </RouterLink>
       </div>
-      <div class="d-flex flex-column flex-1">
-        <VTextField
-          v-model="search"
-          placeholder="Sign up and get 100% off on your first order"
-          density="compact"
-          hide-details="auto"
-        >
-          <template #append>
-            <VBtn variant="outlined" prepend-icon="mdi-magnify" type="submit"
-              >Search</VBtn
-            >
-          </template>
-        </VTextField>
-        <VList class="d-flex suggestions">
-          <VListItem class="py-0 suggestion text-white"
-            >Camping Chair Buy 1 Take 1</VListItem
-          >
-          <VListItem class="py-0 suggestion text-white"
-            >Flat Sandal For Women</VListItem
-          >
-          <VListItem class="py-0 suggestion text-white"
-            >Bra For Women Set 6 Pcs Non Wire</VListItem
-          >
-        </VList>
-      </div>
+
+      <SearchInput class="flex-1" />
 
       <VMenu class="flex-1">
         <template #activator="{ props }">
@@ -93,21 +70,6 @@ header {
 
 .flex-1 {
   flex: 1;
-}
-
-.suggestions {
-  background-color: transparent;
-  font-size: 12px;
-  padding: 0;
-}
-
-.suggestion {
-  min-height: 24px !important;
-}
-
-.v-input :deep(input) {
-  background-color: white;
-  color: black;
 }
 
 a {
