5ee3e2840171369ce7328907595a780b5aedfebf
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-03-28 23:10:28 +0800

    implemented the desktop layout for sign in page

--- pages/signin.vue
+++ pages/signin.vue
@@ -10,8 +10,42 @@
       </VCol>
     </VRow>
   </header>
-  <main class="bg-primary">
-    <VRow></VRow>
+  <main class="bg-primary pa-md-16">
+    <VRow>
+      <VCol cols="12" md="6" class="text-center">
+        <div class="mb-16">
+          <VIcon icon="mdi-store" size="200"></VIcon>
+          <h1 class="font-weight-regular text-h2">Kael Express</h1>
+        </div>
+
+        <p class="text-h5">
+          The leading online shopping platform in Southeast Asia and Taiwan
+        </p>
+      </VCol>
+      <VCol cols="12" md="6" align-self="center">
+        <VCard class="pa-5 mx-auto" max-width="400">
+          <h2 class="text-h6 font-weight-regular mb-5">Log In</h2>
+          <VForm>
+            <VTextField
+              type="email"
+              label="Email Address"
+              placeholder="johndoe@gmail.com"
+              density="compact"
+              class="mb-5"
+            ></VTextField>
+            <VTextField
+              type="password"
+              label="Password"
+              placeholder="*************"
+              density="compact"
+              class="mb-5"
+            ></VTextField>
+            <Button type="submit" size="large" class="w-100 mb-5">Log In</Button>
+          </VForm>
+          <p class="text-center">New to Kael Express? <RouterLink to="/signup" class="text-primary">Sign Up</RouterLink></p>
+        </VCard>
+      </VCol>
+    </VRow>
   </main>
 </template>
 
